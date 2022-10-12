c782ffba3d96de253b26cd01bf1ff23493d23d59
Sohailanjum <msanjum244@gmail.com>
2022-10-12 15:20:28 +0530

    Changes made in main method

--- Demo.java
+++ Demo.java
@@ -2,6 +2,7 @@ class Demo
 {
    public static void main(String[] args)
    {
-      system.out.println("Hello Sohail Welcome to Java Developer course")
+      system.out.println("Hello Sohail Welcome to Java Developer course");
+      system.out.println("Inside main method");
    }
 }
